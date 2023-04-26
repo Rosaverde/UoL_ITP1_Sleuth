944b2d554043e9a4ad942430e8e1c179f02a67cd
Rosaverde <rosaverde.ag@gmail.com>
2023-04-26 12:19:00 +0000

    Case 403 - The Case of the Shiffman Stakeout, Stage 1 - Surveillance

---
+++ 403-0/README.md
@@ -0,0 +1,2 @@
+# Solution
+![alt text](https://github.com/Rosaverde/UoL_ITP1_Sleuth/blob/main/403-0/solution.gif)
---
+++ 403-0/sketch.js
@@ -0,0 +1,66 @@
+/*
+
+CaseNum: 403-0
+
+Case 403 - Surveillance - stage 1
+
+We are on the lookout for the criminal mastermind known as Shiffman. 
+
+- Our sources tell us that he is currently heading east on Reynolds Street. 
+- I need you to sound the alarm if he crosses Adele Street.
+- Shiffman's position is signified by the mouse. 
+- To sound the alarm, draw a MediumTurquoise rectangle covering the remainder of the map from Adele Street to the east.
+
+NB. all road coordinates are measured from their central axis.
+
+Use X11 colours. You can find a reference table at https://www.w3.org/TR/css3-iccprof#numerical.
+
+There are many possible ways of investigating this case, but you should use ONLY the following commands, operators and variables:
+
+  if(){}
+  >
+  <
+  fill()  - Use r,g,b values between 0 and 255.
+  rect()
+  mouseX
+  mouseY
+  width
+  height
+
+*/
+
+var img;
+
+function preload()
+{
+	img = loadImage('map.jpg');
+}
+
+function setup()
+{
+	createCanvas(img.width,img.height);
+}
+
+function draw()
+{
+    // draw the image
+    image(img,0,0);
+
+    //Write your code below here ...
+    if(mouseX > 1340)
+    {   
+    fill(72,209,204)
+    rect(1340, 0, width - 1340, height)
+    }
+
+    // finally, draw Shiffman's position
+    strokeWeight(2);
+    stroke(255);
+    fill(255,0,0);
+    ellipse(mouseX, mouseY, 10, 10);
+
+    // a helpful mouse coordinate pointer
+    fill(255);
+    noStroke();
+    text(`${mouseX},${mouseY}`,mouseX, mouseY);
+}
